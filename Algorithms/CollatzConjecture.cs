55260044c231bc90ec959e1e50fbff232a2ced7a
Ian Pike <Ianpike98@gmail.com>
2022-06-28 17:30:33 -0400

    Update CollatzConjecture.cs

--- Algorithms/CollatzConjecture.cs
+++ Algorithms/CollatzConjecture.cs
@@ -1,4 +1,8 @@
 // Collatz Conjecture
+//    If the number is even, divide it by two.
+//    If the number is odd, triple it and add one.
+//
+//    The conjecture is that these sequences always reach 1, no matter which positive integer is chosen to start the sequence. 
 public static int Collatz(float n, int count=1)
 {
     if (n == 1)

e8cc7b24c653a33834c451f7455bde4c0fa60487
Ian Pike <Ianpike98@gmail.com>
2022-06-29 07:06:22 -0400

    Update MergeSortTests.cs

--- AlgoSharp.UnitTests/MergeSortTests.cs
+++ AlgoSharp.UnitTests/MergeSortTests.cs
@@ -7,9 +7,10 @@ public class MergeSortTests
     [Fact]
     public void MergeSort_Sort_Testing_List_Actually_Sorts_Elements()
     {
-        List<int> unsortedList = new List<int>() {6, 2, 7, 4, 9, 8, 5, 3, 1};
-        List<int> sortedList = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        List<int>? mergeSortedList = MergeSort.Sort(unsortedList) as List<int>;
+        // TODO: Update the unit tests for merge sort so they are not garbage
+        //List<int> unsortedList = new List<int>() {6, 2, 7, 4, 9, 8, 5, 3, 1};
+        //List<int> sortedList = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //List<int>? mergeSortedList = MergeSort.Sort(unsortedList) as List<int>;
         //var parallelMergeSortedList = MergeSort.ParallelSort(unsortedList);
         
         //Assert.NotEmpty(new[] {parallelMergeSortedList});
